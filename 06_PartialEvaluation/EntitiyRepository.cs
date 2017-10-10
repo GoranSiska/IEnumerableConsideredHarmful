43b054a3761cbe60b66cfb3dce5e27babd4667f3
Goran Siska <goran.siska@live.com>
2017-10-10 12:12:07 +0200

    updated partial evaluation example;

---
+++ 06_PartialEvaluation/EntitiyRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_PartialEvaluation
+{
+    public class EntitiyRepository
+    {
+        public IEnumerable<int> ConvertAndStore(IEnumerable<int> items)
+        {
+            AreItemsStored = false;
+            var convertedItems = new List<int>();
+            foreach(var convertedItem in items.Select(Convert))
+            {
+                convertedItems.Add(convertedItem);
+                yield return convertedItem;
+            }
+            Store(convertedItems);
+        }
+
+        public int Convert(int item)
+        {
+            return item * 10;
+        }
+
+        public bool AreItemsStored { get; private set; }
+        public void Store(IEnumerable<int> items)
+        {
+            AreItemsStored = true;
+        }
+    }
+}
---
+++ 06_PartialEvaluation/GivenEntitiyRepository.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace _06_PartialEvaluation
+{
+    [TestFixture]
+    public class GivenEntitiyRepository
+    {
+        [Test]
+        public void WhenConvertAndStore_DataIsStored()
+        {
+            var repository = new EntitiyRepository();
+            Assert.IsFalse(repository.AreItemsStored, "AreItemsStored should be false when no items are stored!");
+            var items = Enumerable.Range(0, 10);
+
+            repository.ConvertAndStore(items).ToList();
+            //repository.ConvertAndStore(items).Take(1).ToList();
+            //repository.ConvertAndStore(items).Where(i=>i==1).ToList();
+            //repository.ConvertAndStore(items).Any(i => i == 1);
+            //repository.ConvertAndStore(items).All(i => i == 2);
+            //repository.ConvertAndStore(items).TakeWhile(i=>i<2).ToList();
+            //repository.ConvertAndStore(items).SkipWhile(i=>i<2).ToList();
+            //repository.ConvertAndStore(items).First();
+            //repository.ConvertAndStore(items).Last();
+            //repository.ConvertAndStore(items).ElementAt(1);
+
+            Assert.IsTrue(repository.AreItemsStored);
+        }
+    }
+}
